94a40188298ab22e019515f48808234a75c3b50e
GideonOdiokine <gidysmartodiokineh@gmail.com>
2022-03-17 02:36:12 +0100

    added Header component and section component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import "./App.css";
+// import Header from "./components/Header";
+import Home from "./components/Home";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      {/* <Header /> */}
+      <Home />
+    </div>
+  );
+};
 
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App"
-import './App.css'
-// take the react component and show it on the screen
-ReactDOM.render(<App />, document.getElementById("root"));
+import App from "./App";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
